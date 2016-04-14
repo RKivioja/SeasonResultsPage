454bed5e7f911ba7b9715e9916b1a005e129df97
Roope Sakari Kivioja <roope.kivioja@gmail.com>
2016-04-15 00:25:27 +0300

    The initial commit.

---
+++ Tulospalvelu/Tulospalvelu.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tulospalvelu.aspx.cs" Inherits="Tulospalvelu.Tulospalvelu" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Styles/Resultstyle.css" rel="stylesheet" type="text/css" />
+    <script src="Scripts/jquery-2.2.3.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            jQuery('.tabs .tab-links a').on('click', function (e) {
+                var currentAttrValue = jQuery(this).attr('href');
+
+                // Show/Hide Tabs
+                jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+                // Change/remove current tab to active
+                jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+                e.preventDefault();
+            });
+        });
+    </script>
+</head>
+
+<body>
+   <h1>Veikkausliigan tulokset kaudelta 2015</h1>
+   <div class="tabs">
+      <ul class="tab-links">
+         <li class="active"><a href="#tab1">Kaikki tulokset</a></li>
+         <li><a href="#tab2">Haku</a></li>
+      </ul>
+ 
+      <div class="tab-content">
+         <div id="tab1" class="tab active">
+            <div id="divAllMatches">
+               <p id="LabelAllMatches">Kaikki pelatut ottelut:</p>
+               <asp:Table ID="TableAllMatches" runat="server"></asp:Table>
+            </div>
+         </div>
+ 
+         <div id="tab2" class="tab">
+            <p>Etsi otteluita päivämäärän perusteella:</p>
+            <form runat="server">
+            <div id="begindate">
+               <label>Alkaen:</label>
+               <asp:TextBox ID="TextBoxStartDay" runat="server"/>
+               <label>(pp.kk.vvvv)</label>
+            </div>
+
+            <div id="enddate">   
+               <label>Päättyen:</label>
+               <asp:TextBox ID="TextBoxEndDay" runat="server"/>
+               <label>(pp.kk.vvvv)</label>
+            </div>
+
+            <asp:ScriptManager runat="server" ID="ScriptManagerSearch">
+            </asp:ScriptManager>
+            
+            <asp:updatepanel ID="UpdatepanelSearch" runat="server">
+               <ContentTemplate>
+                  <asp:Button ID="ButtonSearch" Text="Hae" OnClick="ButtonSearch_Click" runat="server"/>
+                  <asp:Table ID="TableResults" runat="server"></asp:Table>
+               </ContentTemplate>
+            </asp:updatepanel>
+
+            </form>
+
+            
+         </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ Tulospalvelu/Tulospalvelu.aspx.cs
@@ -0,0 +1,213 @@
+﻿/* Football result browser that reads results from a JSON-file and displays them on a webpage. Date-based search-function implemented.
+ * @author: Roope Kivioja
+ * @date: 14.04.2016
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Tulospalvelu
+{
+    public partial class Tulospalvelu : System.Web.UI.Page
+    {
+        private static List<RootObject> matches;
+        private static List<RootObject> filteredMatches;
+
+        /// <summary>
+        /// Just calls the function that is needed to load the results from the file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadMatchFile();
+        }
+
+        /// <summary>
+        /// Loads the results from a file.
+        /// </summary>
+        private void LoadMatchFile()
+        {
+            //string filePath = Path.GetFullPath("matches.json"); 
+            //TODO: KORJAA
+            string filePath = "C:\\Users\\Roope\\Documents\\Koulutus\\AdafyOhjt\\Tulospalvelu\\Tulospalvelu\\matches.json";
+            
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                matches = JsonConvert.DeserializeObject<List<RootObject>>(json);
+            }
+
+            PrintAllMatches(matches, TableAllMatches);
+        }
+
+        /// <summary>
+        /// Prints the full match list to given table.
+        /// </summary>
+        /// <param name="input">List of RootObjects to print.</param>
+        /// <param name="target">The table that will hold the printed matches.</param>
+        private void PrintAllMatches(List<RootObject> input, Table target)
+        {
+            foreach (RootObject match in input)
+            {
+                TableRow MatchRow = new TableRow();
+                target.Rows.Add(MatchRow);
+
+                TableCell DateCell = new TableCell();
+                DateTime starttime = ConvertToDateTime(match.MatchDate.ToString());
+                DateCell.Text = starttime.ToString();
+                MatchRow.Cells.Add(DateCell);
+                
+                TableCell HomeTeamCell = new TableCell();
+                HomeTeamCell.Text = match.HomeTeam.Name.ToString();
+                MatchRow.Cells.Add(HomeTeamCell);
+                TableCell HomeGoalsCell = new TableCell();
+                HomeGoalsCell.Text = match.HomeGoals.ToString();
+                MatchRow.Cells.Add(HomeGoalsCell);
+
+                TableCell SeparatorCell = new TableCell();
+                SeparatorCell.Text = "-";
+                MatchRow.Cells.Add(SeparatorCell);
+
+                TableCell AwayGoalsCell = new TableCell();
+                AwayGoalsCell.Text = match.AwayGoals.ToString();
+                MatchRow.Cells.Add(AwayGoalsCell);
+                TableCell AwayTeamCell = new TableCell();
+                AwayTeamCell.Text = match.AwayTeam.Name.ToString();
+                MatchRow.Cells.Add(AwayTeamCell);
+            }
+        }
+
+        /// <summary>
+        /// The click-event for the search. Searches through the results and prints them.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonSearch_Click(object sender, System.EventArgs e)
+        {
+            string after = TextBoxStartDay.Text.ToString();
+            DateTime afterdate;
+            try
+            {
+                afterdate = ConvertToDateTime(after);
+            }
+            catch
+            {
+                afterdate = new DateTime(2015, 4, 11, 10, 00, 00);
+            }
+
+            string before = TextBoxEndDay.Text.ToString();
+            DateTime beforedate;
+            try
+            {
+                beforedate = ConvertToDateTime(before);
+            }
+            catch
+            {
+                beforedate = new DateTime(2015, 10, 26, 10, 00, 00);
+            }
+
+            var beforeDateX = matches.Where(i => ConvertToDateTime(i.MatchDate) >= afterdate && ConvertToDateTime(i.MatchDate) <= beforedate);
+
+            foreach (RootObject filteredmatch in beforeDateX)
+            {
+                TableRow MatchRow = new TableRow();
+                TableResults.Rows.Add(MatchRow);
+
+                TableCell DateCell = new TableCell();
+                DateTime starttime = ConvertToDateTime(filteredmatch.MatchDate.ToString());
+                DateCell.Text = starttime.ToString();
+                MatchRow.Cells.Add(DateCell);
+
+                TableCell HomeTeamCell = new TableCell();
+                HomeTeamCell.Text = filteredmatch.HomeTeam.Name.ToString();
+                MatchRow.Cells.Add(HomeTeamCell);
+                TableCell HomeGoalsCell = new TableCell();
+                HomeGoalsCell.Text = filteredmatch.HomeGoals.ToString();
+                MatchRow.Cells.Add(HomeGoalsCell);
+
+                TableCell SeparatorCell = new TableCell();
+                SeparatorCell.Text = "-";
+                MatchRow.Cells.Add(SeparatorCell);
+
+                TableCell AwayGoalsCell = new TableCell();
+                AwayGoalsCell.Text = filteredmatch.AwayGoals.ToString();
+                MatchRow.Cells.Add(AwayGoalsCell);
+                TableCell AwayTeamCell = new TableCell();
+                AwayTeamCell.Text = filteredmatch.AwayTeam.Name.ToString();
+                MatchRow.Cells.Add(AwayTeamCell);
+            }
+        }
+
+        /// <summary>
+        /// Converts the date and time strings from the file into a proper DateTime-format.
+        /// </summary>
+        /// <param name="filedate">The date and time string that is being converted.</param>
+        /// <returns>A DateTime-object.</returns>
+        private static DateTime ConvertToDateTime(string filedate)
+        {
+            DateTime converted = Convert.ToDateTime(filedate);
+
+            return converted;
+
+        }
+
+        /// <summary>
+        /// Basic object for holding JSON-data.
+        /// </summary>
+        public class RootObject
+        {
+            public int Id { get; set; }
+            public object Round { get; set; }
+            public int RoundNumber { get; set; }
+            public string MatchDate { get; set; }
+            public HomeTeam HomeTeam { get; set; }
+            public AwayTeam AwayTeam { get; set; }
+            public int HomeGoals { get; set; }
+            public int AwayGoals { get; set; }
+            public int Status { get; set; }
+            public int PlayedMinutes { get; set; }
+            public object SecondHalfStarted { get; set; }
+            public string GameStarted { get; set; }
+            public List<object> MatchEvents { get; set; }
+            public List<object> PeriodResults { get; set; }
+            public bool OnlyResultAvailable { get; set; }
+            public int Season { get; set; }
+            public string Country { get; set; }
+            public string League { get; set; }
+        }
+
+        /// <summary>
+        /// Basic object for holding JSON-data.
+        /// </summary>
+        public class HomeTeam
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string FullName { get; set; }
+            public object Logo { get; set; }
+            public string LogoUrl { get; set; }
+            public int Ranking { get; set; }
+            public string Message { get; set; }
+        }
+
+        /// <summary>
+        /// Basic object for holding JSON-data.
+        /// </summary>
+        public class AwayTeam
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string FullName { get; set; }
+            public object Logo { get; set; }
+            public string LogoUrl { get; set; }
+            public int Ranking { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
